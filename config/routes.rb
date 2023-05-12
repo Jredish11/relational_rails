391356b1cb114448b0b0e4fe7eb4ca3cc61cbd42
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-05-12 14:50:32 -0600

    add get route /grow_rooms/:id  show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
     get "/grow_rooms", to: "grow_rooms#index"
+    get "/grow_rooms/:id", to: "grow_rooms#show"
   # Defines the root path route ("/")
   # root "articles#index"
 end

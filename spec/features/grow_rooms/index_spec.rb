0a6558fabf1ec46e3f5a3f335c78f72b4751739f
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-05-12 14:53:41 -0600

    add controller methods gor grow rooms, adds test for index page

--- spec/features/grow_rooms/index_spec.rb
+++ spec/features/grow_rooms/index_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "/grow_rooms", type: :feature do
   #User Story 1
-  describe "as a visitor, when I visit grow_room index page" do
+  describe "as a visitor, when I visit grow_rooms index page" do
     let!(:room_1) { GrowRoom.create!(name: "Flower Room 1", is_flower: true, plant_capacity: 176)}
     let!(:room_2) { GrowRoom.create!(name: "Veg Room 1", is_flower: false, plant_capacity: 185)}
 
     it "I see the name of each grow_room" do
       visit "/grow_rooms"
-  save_and_open_page
+  # save_and_open_page
       expect(page).to have_content(room_1.name)
-      expect(page).to have_content(room_2.name) #is this okay? i planned to name each grow_room like this
+      expect(page).to have_content(room_2.name) 
 
     end
   end

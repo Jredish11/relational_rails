221ba0438186e190f3e3a1afab5239f9026f8163
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-05-12 17:54:35 -0600

    test: Add tests for strains index page

--- spec/features/strains/index_spec.rb
+++ spec/features/strains/index_spec.rb
@@ -1,3 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe 
+RSpec.describe "/strains", type: :feature do
+  #user story 3
+  describe "as a visitor, when I visit flavors index page" do
+    let!(:room_1) { GrowRoom.create!(name: "Flower Room 1", is_flower: true, plant_capacity: 176)}
+    let!(:room_2) { GrowRoom.create!(name: "Veg Room 1", is_flower: false, plant_capacity: 185)}
+    let!(:flo) { room_1.strains.create!(name: "Flo OG", 
+                                is_sativa: true, 
+                                thc_percentage: 23.04, 
+                                dry_weight: 1500.23,
+                                )}
+    let!(:gary_payton) { room_1.strains.create!(name: "Gary Payton",
+                                        is_sativa: false,
+                                        thc_percentage: 29.69,
+                                        dry_weight: 1323.43,
+                                        )}
+
+    it "should sees each strain in the system and their attributes" do
+      visit "/strains"
+      save_and_open_page
+      expect(page).to have_content(flo.name)
+      expect(page).to have_content(flo.is_sativa)
+      expect(page).to have_content(flo.thc_percentage)
+      expect(page).to have_content(flo.dry_weight)
+
+      expect(page).to have_content(gary_payton.name)
+      expect(page).to have_content(gary_payton.is_sativa)
+      expect(page).to have_content(gary_payton.thc_percentage)
+      expect(page).to have_content(gary_payton.dry_weight)
+
+    end
+  end
+end

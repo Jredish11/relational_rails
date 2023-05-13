7e07f7ceb229761f2fca930b36e87552bd1f0817
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-05-12 21:08:50 -0600

    Test: Add tests for grow_rooms_strains_index page

--- spec/features/strains/index_spec.rb
+++ spec/features/strains/index_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe "/strains", type: :feature do
 
     it "should sees each strain in the system and their attributes" do
       visit "/strains"
-      save_and_open_page
+      # save_and_open_page
       expect(page).to have_content(flo.name)
       expect(page).to have_content(flo.is_sativa)
       expect(page).to have_content(flo.thc_percentage)
@@ -29,6 +29,35 @@ RSpec.describe "/strains", type: :feature do
       expect(page).to have_content(gary_payton.thc_percentage)
       expect(page).to have_content(gary_payton.dry_weight)
 
+    end
+  end
+
+  #User Story 5
+  describe "as a visitor, when I visit grow_rooms strain index page" do
+    let!(:room_1) { GrowRoom.create!(name: "Flower Room 1", is_flower: true, plant_capacity: 176)}
+    let!(:flo) { room_1.strains.create!(name: "Flo OG", 
+      is_sativa: true, 
+      thc_percentage: 23.04, 
+      dry_weight: 1500.23,
+      )}
+    let!(:gary_payton) { room_1.strains.create!(name: "Gary Payton",
+                  is_sativa: false,
+                  thc_percentage: 29.69,
+                  dry_weight: 1323.43,
+                  )}
+                  
+
+    it "I see each Strain that is associated with that Grow_Room with each Strain's attributes" do
+      visit "/grow_rooms/#{room_1.id}/strains"
+      save_and_open_page
+      expect(page).to have_content(flo.name)
+      expect(page).to have_content(flo.is_sativa)
+      expect(page).to have_content(flo.thc_percentage)
+      expect(page).to have_content(flo.dry_weight)
+      expect(page).to have_content(gary_payton.name)
+      expect(page).to have_content(gary_payton.is_sativa)
+      expect(page).to have_content(gary_payton.thc_percentage)
+      expect(page).to have_content(gary_payton.dry_weight)
     end
   end
 end

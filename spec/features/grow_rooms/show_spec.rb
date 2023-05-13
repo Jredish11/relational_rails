5b96ee25446ce4a72802661c1a7e044a8f902e54
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-05-13 09:57:38 -0600

    add md for user story number and clean up

--- spec/features/grow_rooms/show_spec.rb
+++ spec/features/grow_rooms/show_spec.rb
@@ -1,28 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "/grow_rooms/:id", type: :feature do
-  #User Story 2
   describe "as a visitor, when I visit grow_rooms/:id" do
     let!(:room_1) { GrowRoom.create!(name: "Flower Room 1", is_flower: true, plant_capacity: 176)}
     let!(:room_2) { GrowRoom.create!(name: "Veg Room 1", is_flower: false, plant_capacity: 185)}
     let!(:flo) { room_1.strains.create!(name: "Flo OG", 
-                                        is_sativa: true, 
-                                        thc_percentage: 23.04, 
-                                        dry_weight: 1500.23,
-                                        )}
-    let!(:gary_payton) { room_1.strains.create!(name: "Gary Payton",
-                                                is_sativa: false,
-                                                thc_percentage: 29.69,
-                                                dry_weight: 1323.43,
-                                                )}
-    let!(:gg4) { room_2.strains.create!(name: "Gorilla Glue #4",
-                                        is_sativa: false,
-                                        thc_percentage: 29.69,
-                                        dry_weight: 1323.43,
-                                        )}
-
-
-
+      is_sativa: true, 
+      thc_percentage: 23.04, 
+      dry_weight: 1500.23,
+      )}
+      let!(:gary_payton) { room_1.strains.create!(name: "Gary Payton",
+        is_sativa: false,
+        thc_percentage: 29.69,
+        dry_weight: 1323.43,
+        )}
+        let!(:gg4) { room_2.strains.create!(name: "Gorilla Glue #4",
+          is_sativa: false,
+          thc_percentage: 29.69,
+          dry_weight: 1323.43,
+          )}
+          
+          
+          
+    #User Story 2
     it "I see the grow_rooms with that id including the grow_rooms attributes" do
       visit "/grow_rooms/#{room_1.id}"
       expect(page).to have_content(room_1.name)
@@ -35,6 +35,7 @@ RSpec.describe "/grow_rooms/:id", type: :feature do
       expect(page).to have_content(room_2.plant_capacity)
     end
 
+    #User Story 7
     it "displays count of the number of strains associated with this grow_room" do
       visit "/grow_rooms/#{room_1.id}"
       expect(page).to have_content("Number of Strains: 2")
@@ -42,7 +43,6 @@ RSpec.describe "/grow_rooms/:id", type: :feature do
 
       visit "/grow_rooms/#{room_2.id}"
       expect(page).to have_content("Number of Strains: 1")
-
     end
   end
 end
--- spec/features/strains/show_spec.rb
+++ spec/features/strains/show_spec.rb
@@ -1,24 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "/strains/:id", type: :feature do
-  #User story 4
   describe "as a visitor, when I visit /strains/:id" do
     let!(:room_1) { GrowRoom.create!(name: "Flower Room 1", is_flower: true, plant_capacity: 176)}
     let!(:room_2) { GrowRoom.create!(name: "Veg Room 1", is_flower: false, plant_capacity: 185)}
     let!(:flo) { room_1.strains.create!(name: "Flo OG", 
-                                is_sativa: true, 
-                                thc_percentage: 23.04, 
-                                dry_weight: 1500.23,
-                                )}
-    let!(:gary_payton) { room_1.strains.create!(name: "Gary Payton",
-                                        is_sativa: false,
-                                        thc_percentage: 29.69,
-                                        dry_weight: 1323.43,
-                                        )}
-
+      is_sativa: true, 
+      thc_percentage: 23.04, 
+      dry_weight: 1500.23,
+      )}
+      let!(:gary_payton) { room_1.strains.create!(name: "Gary Payton",
+        is_sativa: false,
+        thc_percentage: 29.69,
+        dry_weight: 1323.43,
+        )}
+        
+    #User story 4
     it "should see the strain with that id and their attributes" do
       visit "/strains/#{flo.id}"
-      # save_and_open_page
       expect(page).to have_content(flo.name)
       expect(page).to have_content(flo.is_sativa)
       expect(page).to have_content(flo.thc_percentage)
